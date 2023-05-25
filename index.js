5c75a6210aae19035be26c1fcaa87732fe8c0b23
Devi Nandana <devinandanabk@gmail.com>
2023-05-25 23:20:09 +0530

    added hamburger

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function bookCounselling(){
     window.location = "book_counselling.html";
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.list-options');
+  
+    hamburger.addEventListener('click', function () {
+      menu.classList.toggle('show-menu');
+    });
+});
